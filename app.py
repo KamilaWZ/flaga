c9864763d669d45d9d635f03a771548a369732c6
KamilaWZ <kwalaszczyk.ziomek@gmail.com>
2022-03-14 17:56:11 +0100

    przeniesienie repo na lokalny komp

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from moje_programy.character_wiki import character
+#from moje_programy.open_poem import open_poem
+import random
 
 app=Flask(__name__)
 
@@ -14,6 +17,14 @@ def xd():
 @app.route('/flaga-dla-ukrainy')
 def flaga_dla_ukrainy():
     return render_template("ukraina.html")
+
+#@app.route('/brudnopis')
+#def brudnopis():
+    #super_heroes = ['Bruce Lee', 'Kubuś Puchatek', 'Kopernik', 'Małysz']
+    #chosen_hero = random.choice( super_heroes)
+    #description = character( chosen_hero).encode('utf-8').decode()
+    #poem_lines = open_poem()
+    #return render_template("brudnopis.html", hero=chosen_hero, description=description, poem_lines=poem_lines)
 
 if __name__=="__main__":
     app.run()
